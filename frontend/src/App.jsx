8c21348ab060dfc14c126c7d6d0b580063e72b75
dadan-hidyt <dadanhidyt@gmail.com>
2023-04-18 14:11:31 +0700

    ADADA

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,26 @@
 import { Outlet } from "react-router-dom"
+import Header from "./components/Header";
 import "./index.css";
 function App() {
 
   return (
-    <Outlet/>
+    <>
+      <Header />
+      <div className="flex min-h-full">
+        <aside className="w-2/12 p-2">
+          <ul>
+            <li className=" text-[#5a5a5a] text-[24px] lg:text-lg txt-center text-lg"><a className="hover:bg-[#dedede] rounded cursor-pointer transition-all font-bold block px-5 py-2" href="">🏠&nbsp;<span className="hidden lg:inline">Home</span></a></li>
+            <li className=" text-[#5a5a5a] text-[24px] lg:text-lg txt-center text-lg"><a className="hover:bg-[#dedede] rounded cursor-pointer transition-all font-bold block px-5 py-2" href="">👪&nbsp;<span className="hidden lg:inline">Home</span></a></li>
+            <li className=" text-[#5a5a5a] text-[24px] lg:text-lg txt-center text-lg"><a className="hover:bg-[#dedede] rounded cursor-pointer transition-all font-bold block px-5 py-2" href="">🧭&nbsp;<span className="hidden lg:inline">Home</span></a></li>
+          </ul>
+          <button className="w-full p-3 border border-red-600 mt-5 rounded">MASUK</button>
+
+        </aside>
+        <main className="w-8/12">
+          <Outlet />
+        </main>
+      </div>
+    </>
   )
 }
 
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,8 @@
-import Header from "../components/Header";
 
-export default function Home(){
+export default function Home() {
     return (
-        <Header/>
+        <>
+           <h2>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque, ex. Placeat quibusdam iure ipsum nobis, non facere vero, eaque at autem possimus dolorum labore voluptate, voluptatibus neque exercitationem! Sequi dolorum a sed beatae. Dicta dolorem iusto sunt minus? Reprehenderit, quaerat, fugiat totam dolorum at sequi sed rerum excepturi voluptas ipsam quia aliquid deleniti non dolor necessitatibus perspiciatis cupiditate. Eos saepe aspernatur asperiores accusamus facere repellendus soluta, dignissimos qui ratione a tempore similique rem ducimus repellat, nam labore enim, dolorem commodi. Adipisci ipsa possimus repellendus.</h2> 
+        </>
     );
 }
